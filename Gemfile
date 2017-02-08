09fa5faad8c65d0bee21d7f0c69c5a14a30adf5e
Jason Swett <jason@benfranklinlabs.com>
2017-02-08 17:36:27 -0500

    Add Administrate.

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 
 gem 'devise'
+gem 'administrate', '~> 0.3.0'
+gem 'bourbon'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+resources :authors
+resources :authors_books
+resources :books
+resources :books_tags
+resources :tags
+
+    root to: "users#index"
+  end
+
   resources :tags
   devise_for :users
   resources :authors
