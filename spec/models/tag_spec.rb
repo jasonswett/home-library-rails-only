f97b0257f066f8a8245295f0a2440b2b7b9bb0d0
Jason Swett <jason@benfranklinlabs.com>
2017-02-08 13:44:15 -0500

    Add tests for Tag model.

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @tag = build(:tag)
+  end
+
+  describe "name is not present" do
+    it "is not valid" do
+      @tag.name = ''
+      expect(@tag).not_to be_valid
+    end
+  end
+
+  describe "name is not unique" do
+    it "is not valid" do
+      @tag.save!
+      other_tag = @tag.dup
+      expect(other_tag).not_to be_valid
+    end
+  end
 end
