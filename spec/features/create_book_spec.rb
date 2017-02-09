0a1b15e9c65864d2a567e39d2ed8153c1896115a
Jason Swett <jason@benfranklinlabs.com>
2017-02-09 09:29:24 -0500

    Fix test error.

--- spec/features/create_book_spec.rb
+++ spec/features/create_book_spec.rb
@@ -3,15 +3,13 @@ require "rails_helper"
 RSpec.feature "Creating a book", type: :feature do
   before do
     user = create(:user)
-    create(:author, name: "Carl Sagan")
-
     login_as(user)
 
     @new_book_page = NewBookPage.new
-    @new_book_page.visit
   end
 
   scenario "User creates a new book with title only" do
+    @new_book_page.visit
     @new_book_page.fill_in(name: "Cosmos")
     @new_book_page.submit
 
@@ -19,6 +17,9 @@ RSpec.feature "Creating a book", type: :feature do
   end
 
   scenario "User creates a new book with an author" do
+    create(:author, name: "Carl Sagan")
+
+    @new_book_page.visit
     @new_book_page.fill_in(name: "Cosmos")
     @new_book_page.select_author("Carl Sagan")
     @new_book_page.submit
